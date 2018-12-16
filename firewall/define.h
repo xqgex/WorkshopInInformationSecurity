d3f9f51ac502e6edef29d151f969dbf7b108b6e2
xqgex <xqgex.n@gmail.com>
2018-12-16 21:13:52 +0200

    Makefile

--- firewall/define.h
+++ firewall/define.h
@@ -97,6 +97,32 @@ struct log_node {
 };
 
 //**********************************************************
+//****	Function Declaration				****
+//**********************************************************
+void init_default_rule(void);
+int check_matching_packet_rule(rule_t *, struct iphdr *, __be16, __be16, ack_t);
+long str_to_long(char *);
+rule_t* parse_rule_line(char *);
+__be32 size_to_mask(__u8);
+unsigned int hook_func(unsigned int, struct sk_buff *, const struct net_device *, const struct net_device *, int (*okfn)(struct sk_buff *));
+void delete_rules_array(void);
+static int __init my_module_init_function(void);
+static void __exit my_module_exit_function(void);
+int is_empty(void);
+int insert_first(unsigned char, unsigned char, unsigned char, __be32, __be32, __be16, __be16, reason_t);
+int delete_first(void);
+int write_to_log(unsigned int hooknum, reason_t reason, struct iphdr* ip_header, __be16 src_port, __be16 dst_port, unsigned int action);
+
+static ssize_t rules_read(struct file *, char *, size_t, loff_t *);
+static ssize_t rules_write(struct file *, const char *, size_t, loff_t *);
+static ssize_t log_read(struct file *, char *, size_t, loff_t *);
+ssize_t active_display(struct device *, struct device_attribute *, char *);
+ssize_t active_modify(struct device *, struct device_attribute *, const char *, size_t);
+ssize_t rules_size_display(struct device *, struct device_attribute *, char *);
+ssize_t log_size_display(struct device *, struct device_attribute *, char *);
+ssize_t log_clear_modify(struct device *, struct device_attribute *, const char *, size_t);
+
+//**********************************************************
 //****	Module Variables				****
 //**********************************************************
 static int major_number_rules;
@@ -111,5 +137,18 @@ static struct device* sysfs_device_rules = NULL;
 static struct device* sysfs_device_log = NULL;
 static struct log_node* log_head = NULL;
 static struct nf_hook_ops nfho; // Main hook function
+static struct file_operations fops_rules = {
+	.owner = THIS_MODULE,
+	.read = rules_read,
+	.write = rules_write
+};
+static struct file_operations fops_log = {
+	.owner = THIS_MODULE,
+	.read = log_read
+};
+static DEVICE_ATTR(active,	S_IRWXU	| S_IRWXG | S_IRWXO, active_display,	active_modify);
+static DEVICE_ATTR(rules_size,	S_IRUSR | S_IRGRP | S_IROTH, rules_size_display,NULL);
+static DEVICE_ATTR(log_size,	S_IRUSR | S_IRGRP | S_IROTH, log_size_display,	NULL);
+static DEVICE_ATTR(log_clear,	S_IWUSR	| S_IWGRP | S_IWOTH, NULL,		log_clear_modify);
 
 #endif /* DEFINES_H_ */
--- firewall/fw.h
+++ firewall/fw.h
@@ -4,21 +4,4 @@
 #include "define.h"
 #include "sysfs.h"
 
-//**********************************************************
-//****	Function Declaration				****
-//**********************************************************
-void init_default_rule(void);
-int check_matching_packet_rule(rule_t *, struct iphdr *, __be16, __be16, ack_t);
-long str_to_long(char *);
-rule_t* parse_rule_line(char *);
-__be32 size_to_mask(__u8);
-unsigned int hook_func(unsigned int, struct sk_buff *, const struct net_device *, const struct net_device *, int (*okfn)(struct sk_buff *));
-void delete_rules_array(void);
-static int __init my_module_init_function(void);
-static void __exit my_module_exit_function(void);
-int is_empty(void);
-int insert_first(unsigned char, unsigned char, unsigned char, __be32, __be32, __be16, __be16, reason_t);
-int delete_first(void);
-int write_to_log(unsigned int hooknum, reason_t reason, struct iphdr* ip_header, __be16 src_port, __be16 dst_port, unsigned int action);
-
 #endif // _FW_H_
