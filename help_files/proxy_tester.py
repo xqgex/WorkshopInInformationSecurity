06d0809a7b07cc456d07c006b20ad11132ff09d7
xqgex <xqgex.n@gmail.com>
2019-03-27 20:16:07 +0200

    Last commit

--- help_files/proxy_tester.py
+++ help_files/proxy_tester.py
@@ -9,12 +9,17 @@ BUFFER_SIZE = 4096
 if __name__ == "__main__":
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	for i in range(5):
-		sent = sock.sendto("Test_Test_Test_Test_Test", (PROXY_IP, PROXY_PORT))
-		print "Waiting for answer (sent {})".format(sent)
-		data, server = sock.recvfrom(BUFFER_SIZE)
-		print "Received {} from {}".format(data, server)
-		time.sleep(3)
-	print "closing socket"
-	sock.sendto("Close_Connection", (PROXY_IP, PROXY_PORT))
+#		sent = sock.sendto("4d5a40414243444546474849".decode("hex"), (PROXY_IP, PROXY_PORT)) # XXX
+#		sent = sock.sendto("aaaaaaaaaaaaaaaaaaaaaaaa\r\nContent-Length: 3000\r\nContent-Type: text/plain\r\nContent-aaaa: 0000\r\nContent-bbbb: 1111\r\nContent-Disposition: attachment; filename=\"myfile.ca\"\r\n\r\nsssssssssssssssssssssssss void", (PROXY_IP, PROXY_PORT)) # XXX
+#		sent = sock.sendto("GET /objects?../../../../hacked aaaaaaaaaaaaaaaaaaaaaaaa\r\nContent-Length: 3000\r\nContent-Type: text/plain\r\nContent-aaaa: 0000\r\nContent-bbbb: 1111\r\nContent-Disposition: attachment; filename=\"myfile.ca\"\r\n\r\nsssssssssssssssssssssssss void", (PROXY_IP, PROXY_PORT)) # XXX
+#		sent = sock.sendto("GET /path/to/file.c aaaaaaaaaaaaaaaaaaaaaaaa\r\nContent-Length: 3000\r\nContent-Type: text/plain\r\nContent-aaaa: 0000\r\nContent-bbbb: 1111\r\nContent-Disposition: attachment; filename=\"myfile.ca\"\r\n\r\nsssssssssssssssssssssssss void", (PROXY_IP, PROXY_PORT)) # XXX
+		break # XXX
+#		sent = sock.sendto("Test_Test_Test_Test_Test", (PROXY_IP, PROXY_PORT))
+#		print "Waiting for answer (sent {})".format(sent)
+#		data, server = sock.recvfrom(BUFFER_SIZE)
+#		print "Received {} from {}".format(data, server)
+#		time.sleep(3)
+#	print "closing socket"
+#	sock.sendto("Close_Connection", (PROXY_IP, PROXY_PORT))
 	sock.close()
 
